6f1688cbfa7881f23296a64365e8c38c5326b9ad
R. Bruce Pitt <rbrucepitt@gmail.com>
2018-03-17 15:38:22 -0600

    Footer Skeleton Pages

---
+++ sponsors.php
@@ -0,0 +1,20 @@
+<?php
+// Helping out site Indexx
+// All Rights Reserved, (c) 2018, Anas Gauba, Abdul Gauba, Nathan Schaefer, R. Bruce Pitt
+
+if (!isset($lowerLevel))
+    $lowerLevel = '';
+
+require_once $lowerLevel . 'assets/inc/standard_open.inc.php';
+require_once $lowerLevel . 'assets/inc/standard_header.inc.php';
+?>
+
+
+SPONSOR PAGE
+
+
+
+<?php
+require_once $lowerLevel . 'assets/inc/standard_footer.inc.php';
+require_once $lowerLevel . 'assets/inc/standard_script.inc.php';
+require_once $lowerLevel . 'assets/inc/standard_close.inc.php';
