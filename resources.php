c75602244a1fa51dbe220a1185c0c043872d83c9
Nathan <schaeferNHS@gmail.com>
2018-03-17 11:44:34 -0600

    resources dudes

---
+++ resources.php
@@ -0,0 +1,94 @@
+<html>
+    <title>
+        Resources
+    </title>
+    <body>
+        
+
+        <table>
+            Jobs
+            <tr>
+                <td><a href="test.com">hello</a></td>
+            </tr>
+            <tr>
+                <td><a href=""></a></td>
+            </tr>
+        </table>
+        
+        <table>
+            Start a Business
+            <tr>
+              <td><a href="https://www.cabq.gov/economicdevelopment/business-owners/how-to-start-a-business">
+                   How to Start a Business in Albuquerque</a></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.cabq.gov/planning/business-registration-information">
+                     Business Registration Info</a></td>
+            </tr>
+            <tr>
+                <td><a href="http://www.abqchamber.com/Start_a_business.aspx">
+                     Additional Info</a></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.abqjournal.com/992107/quick-guide-to-starting-a-new-business-in-nm.html">
+                     Quick Guide</a></td>
+            </tr>
+        </table>
+        
+        <table>
+            Free Stuff
+            <tr>
+                <td><a href="https://albuquerque.craigslist.org/search/zip">
+                        Craiglist</a></td>
+            </tr>
+            <tr>
+                        <td><a href="https://www.pennysaverusa.com/classifieds/merchandise/free-stuff/usa/new-mexico/bernalillo/albuquerque/#!/albuquerque-albuquerque/bernalillo-bernalillo/classifieds-classifieds/free-stuff/https:-https:/merchandise-merchandise/new-mexico/radius-2/usa-usa/www.pennysaverusa.com-www.pennysaverusa.com/">
+                             Pennysaver</a></td>
+            </tr>
+            <tr>
+                <td><a href="https://us.letgo.com/en/i/free-stuff_b19c6af1-a61e-4793-8722-11bcd067436a">
+                     LetGo</a></td>
+            </tr>
+            <tr>
+                <td><a href="https://trashnothing.com/albuquerque-freecycle">
+                     TrashNothing</a></td>
+            </tr>
+            
+        </table>
+        
+        <table>
+            Civic Support
+            <tr>
+                        <td><a href="https://www.alignable.com/albuquerque-nm/directory/alpha/civic-groups-and-communities">
+                             Civic Groups and Communities Businesses in Albuquerque</a></td>
+            </tr>
+            <tr>
+                <td><a href=""></a></td>
+            </tr>
+        </table>
+        
+        <table>
+            Educational Resources
+            <tr>
+                    <td><a href="https://datausa.io/profile/geo/albuquerque-nm/">
+                         Albuquerque Statistics</a></td>
+            </tr>
+            <tr>
+                <td><a href=""></a></td>
+            </tr>
+                        <tr>
+                <td><a href=""></a></td>
+            </tr>
+                       <tr>
+                <td><a href=""></a></td>
+            </tr>
+                        <tr>
+                <td><a href=""></a></td>
+            </tr>
+                        <tr>
+                <td><a href=""></a></td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
