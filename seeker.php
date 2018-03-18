37898516cb7a25e02c970b82ec091556c77d5608
Muhammad Anas Gauba <mgauba@unm.edu>
2018-03-18 10:29:53 -0600

    modified

--- seeker.php
+++ seeker.php
@@ -62,7 +62,7 @@ include "fakerequest.php";
             <div class="row">
                 <div class="col-sm-2 plan price-one wow fadeInDown" data-wow-offset="0" data-wow-delay="0.2s">
                     <ul>
-                    <li class="heading-two">
+                    <li class="heading-six">
 						<h2>Date</h2>
                     </li>
 					<?php
@@ -76,7 +76,7 @@ include "fakerequest.php";
                 </div>
                 <div class="col-sm-5 plan price-one wow fadeInDown" data-wow-offset="0" data-wow-delay="0.2s">
                     <ul>
-                    <li class="heading-two">
+                    <li class="heading-three">
                     <h2>Title</h2>
 					<?php
 					foreach ($seeker as $id => $var ) 
