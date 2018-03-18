025dfcbbf6c13ac15569c63251edf41c032370a7
Muhammad Anas Gauba <mgauba@unm.edu>
2018-03-18 10:59:20 -0600

    modified sponsors

--- sponsors.php
+++ sponsors.php
@@ -9,16 +9,18 @@ require_once $lowerLevel . 'assets/inc/standard_open.inc.php';
 require_once $lowerLevel . 'assets/inc/standard_header.inc.php';
 ?>
 
-	<div id="feature">
-		<div class="container">
-			<div class="row">
-				<div class="text-center">
-                                    <h3>SPONSOR PAGE</h3>
-				</div>
-			</div>
-		</div>
+<div id="feature">
+    <div class="container">
+	<div class="row">
+            <div class="text-center">
+                <h3>OUR SPONSORS</h3>
+                <img src="img/cnm-stemulus.png" align="left"width="400" height="150">
+                <img src="img/deep-dive-logo.png" align="middle"width="300" height="150">
+                <img src="img/RSI-logo.jpg" align="right"width="300" height="150">
+            </div>
 	</div>
-
+    </div>
+</div>
 
 
 
